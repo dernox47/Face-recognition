94a9baccbb45807341e25f56eb82275088dfdaae
András Szin <andras.szin47@gmail.com>
2022-05-22 12:11:05 +0200

    Update
    - felirat készítése a keret fölé

--- cam.py
+++ cam.py
@@ -1,7 +1,7 @@
 import cv2
 
-face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
-eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+face_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade/haarcascade_eye.xml')
 
 cap = cv2.VideoCapture(1)
 
@@ -12,10 +12,15 @@ while True:
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 0, 255), 2)
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        name = "unknown"
+        color = (255, 255, 255)
+        stroke = 2
+        cv2.putText(frame, name, (x, y), font, 1, color, stroke, cv2.LINE_AA)
 
-    eye_faces = eye_cascade.detectMultiScale(gray, 1.1, 4)
-    for (px, py, pw, ph) in eye_faces:
-        cv2.rectangle(frame, (px, py), (px+pw, py+ph), (255, 0, 0), 2)
+        eye_faces = eye_cascade.detectMultiScale(gray, 1.1, 4)
+        for (px, py, pw, ph) in eye_faces:
+            cv2.rectangle(frame, (px, py), (px+pw, py+ph), (255, 0, 0), 2)
 
     cv2.imshow('frame', frame)
     key = cv2.waitKey(30)
